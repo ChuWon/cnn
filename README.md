e64a823a77d74a5f3f25cd8fafccbb4e5b9743d4
ChuWon <chudon69420@gmail.com>
2025-12-12 14:47:51 +0530

    multithreading

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # cnn
 convolutional neural network from scratch in js moment xp
 
-97% acc for kittyalious XD
+- 97% acc for kittyalious XD
+- bun.js + workerMain.js = 40% faster for kitty XDXD. 10% faster on nodejs & web.
 
 incomplete project XD
--- runWorker.js
+++ runWorker.js
@@ -1,26 +1,94 @@
-/*
+const file = 'mnist_train.csv';
 
-O = I - K + 1
-grad = OxO
-bias_grad = OxO
-kernel_grad = KxK
-input_grad = IxI
+let cpuCount;
+if (typeof window === 'undefined') {
+	globalThis.Worker = require('worker_threads').Worker;
+	cpuCount = require('os').cpus().length;
 
-bias_grad = grad
-kernel_grad = correlate(input, grad)
-I - O + 1
-I - (I - K + 1) + 1
-I - I + K - 1 + 1 = K
+	require('fs/promises').readFile(file, { encoding: 'utf8' }).then(parse);
+} else {
+	cpuCount = navigator.hardwareConcurrency;
+	fetch(file).then(res => res.text()).then(parse);
+}
 
-input_grad = convolve(grad, kernel) = correlate(grad, rot180(kernel))
-I - K + 1 + (K - 1) = I
+const workerCount = Math.max(1, cpuCount - 2);
+console.log(`worker count: ${workerCount}`);
 
-convolve:
-expand input by border of zeroes with thickness (kernelSize - 1) & compute correlation in reverse.
-E = I + (K - 1) * 2
-Y = E - K + 1 = I + 2*K - 2 - K + 1 = I + K - 1
+const workers = [];
 
-*/
+for (let i = 0; i < workerCount; i++) {
+	const worker = new Worker('./worker.js');
+	worker.onmessage = onMessageWeb;
+	worker.on && worker.on('message', onMessage);
+	workers.push(worker);
+}
+
+function onMessageWeb(event) {
+	onMessage(event.data);
+}
+
+function onMessage(msg) {
+	switch (msg.id) {
+		case 'paramsUpdated':
+			if (--pendingParamUpdates <= 0) {
+				paramUpdate.resolve();
+			}
+			break;
+
+		case 'executed':
+			tasks[msg.taskId].resolve(msg.result);
+			break;
+
+		default:
+			console.log(`unknown msg from worker: ${msg.id}`);
+	}
+}
+
+let paramUpdate;
+let pendingParamUpdates = 0;
+
+function updateParams(params, grad) {
+	pendingParamUpdates++;
+
+	const worker = workers[Math.floor(Math.random() * workers.length)];
+	worker.postMessage({
+		id: 'updateParams', 
+		params, 
+		grad, 
+		learningRate
+	});
+
+	paramUpdate = {}
+	paramUpdate.promise = new Promise(resolve => {
+		paramUpdate.resolve = resolve;
+	});
+}
+
+const tasks = {};
+
+let workerIndex = 0;
+function execute(name, args) {
+	const taskId = Math.floor(Math.random() * 4666_420_69969);
+
+	const task = {};
+	task.promise = new Promise((resolve, reject) => {
+		task.resolve = resolve;
+		task.reject = reject;
+	});
+	tasks[taskId] = task;
+
+	const worker = workers[workerIndex];
+	workerIndex = (workerIndex + 1) % workers.length;
+
+	worker.postMessage({
+		id: 'execute', 
+		taskId, 
+		name, 
+		args
+	});
+
+	return task.promise;
+}
 
 class Conv {
 	constructor(inputSize, inputDepth, kernelSize, depth) {
@@ -37,23 +105,19 @@ class Conv {
 		this.biases = createParams(this.outputLength, 0.01);
 	}
 
-	forward(x) {
+	async forward(x) {
 		this.x = x;
-		return correlate(this.inputSize, this.inputDepth, this.kernelSize, this.depth, x, this.kernels, this.biases);
+		return await execute('correlate', [this.inputSize, this.inputDepth, this.kernelSize, this.depth, x, this.kernels, this.biases]);
 	}
 
-	backward(grad) {
-		const numSamples = grad.length / this.outputLength;
+	async backward(grad) {
+		let kernelGrad = execute('correlate', [this.inputSize, this.inputDepth, this.outputSize, this.depth, this.x, grad]);
+		let inputGrad = execute('convolve', [this.outputSize, this.inputDepth, this.kernelSize, this.depth, grad, this.kernels]);
+		let biasGrad = execute('sum', [grad, this.outputLength]);
 
-		const kernelGrad = correlate(this.inputSize, this.inputDepth, this.outputSize, this.depth, this.x, grad);
-		const inputGrad = convolve(this.outputSize, this.inputDepth, this.kernelSize, this.depth, grad, this.kernels);
-		const biasGrad = new Float32Array(this.biases.length);
-
-		for (let i = 0; i < this.outputLength; i++) {
-			for (let n = 0; n < numSamples; n++) {
-				biasGrad[i] += grad[n * this.outputLength + i];
-			}
-		}
+		kernelGrad = await kernelGrad;
+		inputGrad = await inputGrad;
+		biasGrad = await biasGrad;
 
 		updateParams(this.kernels, kernelGrad);
 		updateParams(this.biases, biasGrad);
@@ -68,18 +132,18 @@ class Activation {
 		this.fPrime = fPrime;
 	}
 
-	forward(x) {
+	async forward(x) {
 		this.x = x;
 
-		const out = new Float32Array(x.length);
+		const out = FloatArray(x.length);
 		for (let i = 0; i < x.length; i++) {
 			out[i] = this.f(x[i]);
 		}
 		return out;
 	}
 
-	backward(grad) {
-		const out = new Float32Array(grad.length);
+	async backward(grad) {
+		const out = FloatArray(grad.length);
 		for (let i = 0; i < grad.length; i++) {
 			out[i] = this.fPrime(this.x[i]) * grad[i];
 		}
@@ -119,11 +183,11 @@ class Linear {
 		this.biases = createParams(outputLength);
 	}
 
-	forward(x) {
+	async forward(x) {
 		this.x = x;
 
 		const numSamples = x.length / this.inputLength;
-		const out = new Float32Array(numSamples * this.outputLength);
+		const out = FloatArray(numSamples * this.outputLength);
 
 		for (let n = 0; n < numSamples; n++) {
 			for (let o = 0; o < this.outputLength; o++) {
@@ -138,38 +202,14 @@ class Linear {
 		return out;
 	}
 
-	backward(grad) {
-		const numSamples = grad.length / this.outputLength;
+	async backward(grad) {
+		let weightGrad = execute('linearWeightGrad', [this.inputLength, this.outputLength, this.x, grad]);
+		let biasGrad = execute('sum', [grad, this.outputLength]);
+		let inputGrad = execute('linearInputGrad', [this.inputLength, this.outputLength, this.weights, grad]);
 
-		const weightGrad = new Float32Array(this.weights.length);
-
-		for (let i = 0; i < this.outputLength; i++) {
-			for (let j = 0; j < this.inputLength; j++) {
-				const ni = i * this.inputLength + j;
-				for (let k = 0; k < numSamples; k++) {
-					weightGrad[ni] += grad[i + k * this.outputLength] * this.x[j + k * this.inputLength];
-				}
-			}
-		}
-
-		const biasGrad = new Float32Array(this.biases.length);
-
-		for (let i = 0; i < this.outputLength; i++) {
-			for (let j = 0; j < numSamples; j++) {
-				biasGrad[i] += grad[j * this.outputLength + i];
-			}
-		}
-
-		const inputGrad = new Float32Array(numSamples * this.inputLength);
-
-		for (let i = 0; i < this.inputLength; i++) {
-			for (let j = 0; j < numSamples; j++) {
-				const ni = j * this.inputLength + i;
-				for (let k = 0; k < this.outputLength; k++) {
-					inputGrad[ni] += this.weights[k * this.inputLength + i] * grad[j * this.outputLength + k];
-				}
-			}
-		}
+		weightGrad = await weightGrad;
+		biasGrad = await biasGrad;
+		inputGrad = await inputGrad;
 
 		updateParams(this.weights, weightGrad);
 		updateParams(this.biases, biasGrad);
@@ -190,11 +230,11 @@ class MaxPool {
 		this.outputLength = this.outputSize * this.outputSize;
 	}
 
-	forward(x) {
+	async forward(x) {
 		const numSamples = x.length / this.inputLength;
 
-		const out = new Float32Array(numSamples * this.outputLength);
-		this.maxIndex = new Uint32Array(out.length);
+		const out = FloatArray(numSamples * this.outputLength);
+		this.maxIndex = new Uint32Array(new SharedArrayBuffer(4 * out.length));
 
 		for (let n = 0; n < numSamples; n++) {
 			for (let oy = 0; oy < this.outputSize; oy++) {
@@ -227,10 +267,10 @@ class MaxPool {
 		return out;
 	}
 
-	backward(grad) {
+	async backward(grad) {
 		const numSamples = grad.length / this.outputLength;
 
-		const inputGrad = new Float32Array(numSamples * this.inputLength);
+		const inputGrad = FloatArray(numSamples * this.inputLength);
 
 		for (let n = 0; n < numSamples; n++) {
 			for (let oy = 0; oy < this.outputSize; oy++) {
@@ -252,105 +292,15 @@ class MaxPool {
 }
 
 function createParams(n, f = 1) {
-	return Float32Array.from({ length: n }, () => (Math.random() - 0.5) * f);
-}
-
-function updateParams(params, grad) {
-	for (let i = 0; i < params.length; i++) {
-		params[i] -= grad[i] * learningRate;
+	const out = FloatArray(n);
+	for (let i = 0; i < out.length; i++) {
+		out[i] = (Math.random() - 0.5) * f;
 	}
-}
-
-function correlate(inputSize, inputDepth, kernelSize, depth, x, kernels, biases) {
-	const outputSize = inputSize - kernelSize + 1;
-	const inputLength = inputDepth * inputSize * inputSize;
-	const outputLength = depth * inputDepth * outputSize * outputSize;
-
-	const numSamples = x.length / inputLength;
-	const out = new Float32Array(biases ? numSamples * outputLength : outputLength);
-
-	for (let n = 0; n < numSamples; n++) {
-		for (let d = 0; d < depth; d++) {
-			for (let i = 0; i < inputDepth; i++) {
-				for (let oy = 0; oy < outputSize; oy++) {
-					for (let ox = 0; ox < outputSize; ox++) {
-						const bi = (d * inputDepth * outputSize * outputSize) + 
-							(i * outputSize * outputSize) + 
-							(oy * outputSize + ox);
-						
-						let ni = bi;
-						if (biases) {
-							ni += n * outputLength;
-							out[ni] = biases?.[bi] || 0;
-						}
-
-						for (let ky = 0; ky < kernelSize; ky++) {
-							for (let kx = 0; kx < kernelSize; kx++) {
-								const xi = n * inputLength + 
-									(i * inputSize * inputSize) + 
-									(oy + ky) * inputSize + (ox + kx);
-								const ki = (d * inputDepth * kernelSize * kernelSize) + 
-									(i * kernelSize * kernelSize) + 
-									(ky * kernelSize + kx);
-								out[ni] += x[xi] * kernels[ki];
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-
 	return out;
 }
 
-function convolve(inputSize, inputDepth, kernelSize, depth, x, kernels, biases) {
-	const outputSize = inputSize + kernelSize - 1;
-	const inputLength = inputDepth * inputSize * inputSize;
-	const outputLength = depth * inputDepth * outputSize * outputSize;
-	const borderSize = kernelSize - 1;
-
-	const numSamples = x.length / inputLength;
-	const out = new Float32Array(biases ? numSamples * outputLength : outputLength);
-
-	for (let n = 0; n < numSamples; n++) {
-		for (let d = 0; d < depth; d++) {
-			for (let i = 0; i < inputDepth; i++) {
-				for (let oy = 0; oy < outputSize; oy++) {
-					for (let ox = 0; ox < outputSize; ox++) {
-						const bi = (d * inputDepth * outputSize * outputSize) + 
-							(i * outputSize * outputSize) + 
-							(oy * outputSize + ox);
-						
-						let ni = bi;
-						if (biases) {
-							ni += n * outputLength;
-							out[ni] = biases?.[bi] || 0;
-						}
-
-						for (let ky = 0; ky < kernelSize; ky++) {
-							for (let kx = 0; kx < kernelSize; kx++) {
-								const inputX = ox + kx - borderSize;
-								const inputY = oy + ky - borderSize;
-
-								if (inputX >= 0 && inputX < inputSize && inputY >= 0 && inputY < inputSize) {
-									const xi = n * inputLength + 
-										(i * inputSize * inputSize) + 
-										inputY * inputSize + inputX;
-									const ki = (d * inputDepth * kernelSize * kernelSize) + 
-										(i * kernelSize * kernelSize) + 
-										(kernelSize - 1 - ky) * kernelSize + (kernelSize - 1 - kx);
-									out[ni] +=  x[xi] * kernels[ki];
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-
-	return out;
+function FloatArray(n) {
+	return new Float32Array(new SharedArrayBuffer(4 * n));
 }
 
 function softmax(x, outputLength) {
@@ -398,7 +348,7 @@ function crossEntropy(targets, predictions, outputLength) {
 function softmaxCrossEntropyPrime(targets, predictions, outputLength) {
 	const numSamples = targets.length / outputLength;
 
-	const out = new Float32Array(targets.length);			
+	const out = FloatArray(targets.length);			
 	for (let i = 0; i < targets.length; i++) {
 		out[i] = (predictions[i] - targets[i]) / numSamples;
 	}
@@ -427,77 +377,6 @@ function getAccuracy(targets, predictions, outputLength) {
 
 	const n = targets.length / outputLength;
 	return correct / n;
-}
-
-function convolveXD() {
-	console.log('>>> TESTING CONVOLVE >>>');
-
-	const x = [
-		1, 2, 3, 4, 
-		5, 6, 7, 8, 
-		9, 10, 11, 12, 
-		13, 14, 15, 16
-	];
-
-	const kernel = [
-		1, -4, 2,  
-		3, 5, 0, 
-		3, 2, 2
-	];
-
-	const target = [
-		1, -2, -3, -4, -10, 8, 
-		8, -3, 12, 19, 2, 16, 
-		27, 25, 55, 69, 28, 32, 
-		55, 65, 111, 125, 56, 48, 
-		66, 155, 186, 201, 126, 24, 
-		39, 68, 99, 106, 62, 32
-	];
-
-	console.log(`INPUT: [${x}]`);
-	console.log(`KERNEL: [${kernel}]`);
-	console.log(`TARGET: [${target}]`);
-
-	const inputSize = 4;
-	const kernelSize = 3;
-	const outputSize = inputSize + kernelSize - 1;
-	const borderSize = kernelSize - 1;
-
-	const out = new Float32Array(outputSize * outputSize);
-
-	for (let oy = 0; oy < outputSize; oy++) {
-		for (let ox = 0; ox < outputSize; ox++) {
-			const ni = oy * outputSize + ox;
-
-			for (let ky = 0; ky < kernelSize; ky++) {
-				for (let kx = 0; kx < kernelSize; kx++) {
-					const inputX = ox + kx - borderSize;
-					const inputY = oy + ky - borderSize;
-
-					if (inputX >= 0 && inputX < inputSize && inputY >= 0 && inputY < inputSize) {
-						const xi = inputY * inputSize + inputX;
-						const ki = (kernelSize - 1 - ky) * kernelSize + (kernelSize - 1 - kx);
-						out[ni] +=  x[xi] * kernel[ki];
-					}
-				}
-			}
-		}
-	}
-
-	console.log(`OUTPUT: [${out}]`);
-
-	const out2 = convolve(inputSize, 1, kernelSize, 1, x, kernel);
-	console.log(`OUTPUT2: [${out2}]`);
-
-	for (let i = 0; i < target.length; i++) {
-		if (target[i] !== out[i] || target[i] !== out2[i]) {
-			console.log(`TEST FAILED OwO!!!\nINDEX: ${i}\nTARGET: ${target[i]}\nVALUE: ${out[i]}\nVALUE2: ${out2[i]}`);
-			return false;
-		}
-	}
-
-	console.log(`TEST PASSED XD!`);
-	return true;
 }
 
 // dataset
@@ -547,9 +426,9 @@ function createDatasets(dataSplit, trainSplit) {
 	return { train, val };
 }
 
-function prepareData(data) {
-	const x = new Float32Array(data.length * inputLength);
-	const y = new Uint8Array(data.length * outputLength);
+function prepareData(data, log = true) {
+	const x = FloatArray(data.length * inputLength);
+	const y = new Uint8Array(new SharedArrayBuffer(data.length * outputLength));
 
 	const counter = {};
 
@@ -561,15 +440,17 @@ function prepareData(data) {
 		counter[item.y] = (counter[item.y] || 0) + 1;
 	}
 
-	let text = `${data.length} total samples:\n`;
+	if (log) {
+		let text = `${data.length} total samples:\n`;
 
-	for (const key in counter) {
-		const n = counter[key];
-		const percent = n / data.length * 100;
-		text += `${key} / ${n} / ${percent.toFixed(2)}%\n`;
+		for (const key in counter) {
+			const n = counter[key];
+			const percent = n / data.length * 100;
+			text += `${key} / ${n} / ${percent.toFixed(2)}%\n`;
+		}
+
+		console.log(text);
 	}
-
-	console.log(text);
 
 	return [x, y];
 }
@@ -604,7 +485,7 @@ const epochs = 10;
 const networks = {
 	cnn: {
 		batchSize: 1, 
-		learningRate: 0.01, 
+		learningRate: 0.001, 
 		layers: () => [
 			new Conv(28, 1, 7, 16), 
 			new ReLU(), 
@@ -633,7 +514,7 @@ inspect(layers);
 const inputLength = layers[0].inputLength;
 const outputLength = layers[layers.length - 1].outputLength;
 
-function train() {
+async function train() {
 	const [trainX, trainY] = datasets.train;
 	const [valX, valY] = datasets.val;
 
@@ -645,21 +526,23 @@ function train() {
 		for (let i = 0; i < trainCount; i += batchSize) {
 			const batchStartTime = performance.now();
 
-			const batchX = trainX.slice(i * inputLength, (i + batchSize) * inputLength);
-			const batchY = trainY.slice(i * outputLength, (i + batchSize) * outputLength);
+			const [batchX, batchY] = prepareData(data.slice(i, i + batchSize), false);
 
-			const preds = forward(batchX);
-			backward(batchY, preds);
+			const preds = await forward(batchX);
+			await backward(batchY, preds);
 
 			const f = Math.min(1, (i + batchSize) / trainCount);
-			console.log(`epoch ${e + 1}: ${(f * 100).toFixed(2)}%, acc: ${(getAccuracy(batchY, preds, outputLength) * 100).toFixed(2)}%, time: ${((performance.now() - batchStartTime) / 1000).toFixed(3)}s`);
+			const accuracy = getAccuracy(batchY, preds, outputLength);
+			console.log(`epoch ${e + 1}: ${(f * 100).toFixed(4)}%, acc: ${(accuracy * 100).toFixed(2)}%, time: ${((performance.now() - batchStartTime) / 1000).toFixed(3)}s`);
+
+			await paramUpdate.promise;
 		}
 
-		const trainPreds = forward(trainX);
+		const trainPreds = await forward(trainX);
 		const trainLoss = crossEntropy(trainY, trainPreds, outputLength);
 		const trainAccuracy = getAccuracy(trainY, trainPreds, outputLength);
 
-		const valPreds = forward(valX);
+		const valPreds = await forward(valX);
 		const valLoss = crossEntropy(valY, valPreds, outputLength);
 		const valAccuracy = getAccuracy(valY, valPreds, outputLength);
 
@@ -669,30 +552,18 @@ function train() {
 	}
 }
 
-function forward(x) {
+async function forward(x) {
 	let y = x;
 	for (let i = 0; i < layers.length; i++) {
-		y = layers[i].forward(y);
+		y = await layers[i].forward(y);
 	}
 	y = softmax(y, layers[layers.length - 1].outputLength);
 	return y;
 }
 
-function backward(targets, predictions) {
+async function backward(targets, predictions) {
 	let grad = softmaxCrossEntropyPrime(targets, predictions, outputLength);
 	for (let i = layers.length - 1; i >= 0; i--) {
-		grad = layers[i].backward(grad);
+		grad = await layers[i].backward(grad);
 	}
-}
-
-convolveXD();
-
-const file = 'mnist_train.csv';
-
-if (typeof window === 'undefined') {
-	const fs = require('fs');
-	const text = fs.readFileSync(file, { encoding: 'utf8' });
-	parse(text);
-} else {
-	fetch(file).then(res => res.text()).then(parse);
 }
